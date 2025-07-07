3639a9e2eda857ee87893fc41c740f1d7449eca9
git config --global user.name <cirillo773@gmail.com>
2025-07-07 16:50:16 +0200

    aggiunti file jar e modificato i file bat

--- MainStart.bat
+++ MainStart.bat
@@ -1,9 +1,1 @@
-@echo off
-
-rem Compila tutto da src mantenendo package e output in bin
-javac -d bin src/parametri/*.java src/frames/*.java
-
-rem Esegue la classe Main dentro il package frames
-java -cp ".;bin" frames.Main
-
-pause
+java -cp "BookRecommender.jar" bookrecommender.Main
